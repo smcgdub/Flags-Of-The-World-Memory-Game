08fbb983c8c4e0dd7802e863d8fdfa3facc4f5e7
smcgdub <stephen_xyz1@hotmail.com>
2021-02-26 16:16:24 +0000

    Have added the function in JS so the dark/light mode switch works

---
+++ app.js
@@ -0,0 +1,9 @@
+// VARIABLES 
+let buttons = document.querySelectorAll('button')
+console.log(button)
+
+// FUNCTIONS
+function darkMode(){
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
